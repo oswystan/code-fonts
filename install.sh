540045790ae1b7b5f595721769c38711008952ed
oswystan <oswystan@126.com>
2017-07-04 22:59:48 +0800

    feature: add install.sh for ubuntu

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+###########################################################################
+##                     Copyright (C) 2017 wystan
+##
+##       filename: install.sh
+##    description:
+##        created: 2017-07-04 22:55:53
+##         author: wystan
+##
+###########################################################################
+
+function check_os() {
+    os=`uname -s`
+    [ $os = "Darwin" ] && exit 0
+}
+
+function install_font() {
+    sudo cp ./*.ttf /usr/share/fonts/
+    sudo mkfontscale
+    sudo mkfontdir
+    sudo fc-cache -fv
+}
+
+check_os
+install_font
+###########################################################################
